6cd8a40365ebd05a4aea26edf5caecfa765dab87
ythy <ythycn@hotmail.com>
2019-06-03 16:27:14 +0800

    remove MediaActivity from recent task list

---
+++ app/src/main/java/com/mx/easytouch/activity/InstalledActivity.kt
@@ -0,0 +1,13 @@
+package com.mx.easytouch.activity
+
+import android.app.Activity
+import android.os.Bundle
+import com.mx.easytouch.R
+
+class InstalledActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_installed)
+    }
+}
